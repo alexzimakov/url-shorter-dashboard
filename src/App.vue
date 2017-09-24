122e4bd221e3da14bf5835e273a464f59417772e
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-24 19:49:41 +0300

    Define global style

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,17 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+  background: white;
+  font-weight: normal;
+  font-style: normal;
+  font-family: 'system-ui';
+
+  -webkit-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+}
 </style>
