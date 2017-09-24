ede7cb78af3fdb1832f0000d5627838e16281928
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-24 19:51:24 +0300

    Add empty Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <wrapper>
+    <l-header></l-header>
+    <wrapper-inner>
+      <router-view></router-view>
+    </wrapper-inner>
+    <l-footer></l-footer>
+  </wrapper>
+</template>
+
+<script>
+  import { getComponentsObject } from '@/utils';
+  import Wrapper from './Wrapper';
+  import WrapperInner from './WrapperInner';
+  import Header from './Header';
+  import Footer from './Footer';
+
+  export default {
+    name: 'app',
+    components: getComponentsObject([
+      Wrapper,
+      WrapperInner,
+      Header,
+      Footer,
+    ]),
+  };
+</script>
+
+<style>
+</style>
