a98ba1696fb5b99b8b2606ee430258f2afdb14e7
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-24 16:36:28 +0300

    Add WrapperInner component

---
+++ src/components/WrapperInner.vue
@@ -0,0 +1,20 @@
+<script>
+  export default {
+    name: 'wrapper-inner',
+    render(createElement) {
+      return createElement(
+        'div',
+        {
+          class: ['wrapper-inner'],
+        },
+        this.$slots.default,
+      );
+    },
+  };
+</script>
+
+<style>
+  .wrapper-inner {
+    flex: 1;
+  }
+</style>
