9d26830e4f7a6b34128f850bd9f1d5d4ee695314
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-26 22:21:21 +0300

    Setting vue-resource

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';
 import App from './App';
 import store from './store';
 import router from './router';
-// Add support of Font Awesome icons.
 import './font-awesome-icons.less';
 
 Vue.config.productionTip = false;
+Vue.use(VueResource);
+Vue.http.options.root = 'http://localhost:5000';
 Vue.use(ElementUI);
 
 /* eslint-disable no-new */
