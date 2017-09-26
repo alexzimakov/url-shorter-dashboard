7e7ac36460f46e027ef1c1e71d0667e55877faba
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-26 22:22:32 +0300

    Update App component

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,26 @@
 <script>
 export default {
   name: 'app',
+  methods: {
+    showOfflineNotification() {
+      this.$message({
+        message: 'Нет подключения к Интернету.',
+        type: 'warning',
+        showClose: true,
+      });
+    },
+  },
+  mounted() {
+    if (!navigator.onLine) {
+      this.showOfflineNotification();
+    }
+
+    window.addEventListener('offline', this.showOfflineNotification);
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
 html, body {
   margin: 0;
   padding: 0;
@@ -23,5 +39,26 @@ html, body {
   -moz-text-size-adjust: 100%;
   -ms-text-size-adjust: 100%;
   -webkit-font-smoothing: antialiased;
+}
+
+.help {
+  color: #8492A6;
+  font-size: 14px;
+
+  &.is-danger {
+    color: #FF4949;
+  }
+}
+
+.is-danger input {
+  border-color: #FF4949;
+}
+
+.errors-box {
+  margin-bottom: 16px;
+  padding: 8px 16px;
+  border-radius: 5px;
+  background: #FF4949;
+  color: #FFFFFF;
 }
 </style>
