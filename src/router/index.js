1f1366433eab4ec293a445af66229a54aa4d8c1f
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-26 22:23:11 +0300

    Update router settings

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import { requireGuest } from '@/utils/auth';
 import Home from '@/components/Home';
+import Register from '@/components/Register';
+import Login from '@/components/Login';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      beforeEnter: requireGuest,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      beforeEnter: requireGuest,
     },
   ],
 });
