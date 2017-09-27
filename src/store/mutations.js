0203cf80ff5e17a3acfd5d8dc9c1df44e074562f
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-27 22:34:14 +0300

    Update mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -5,4 +5,17 @@ export default {
     Vue.set(state, 'session', payload);
     localStorage.setItem('session', JSON.stringify(payload));
   },
+  updateLinks(state, payload = []) {
+    payload.forEach((rawLink) => {
+      const link = rawLink;
+      const id = link._id;
+
+      delete link._id;
+
+      Vue.set(state.links, id, rawLink);
+    });
+  },
+  deleteLink(state, id) {
+    Vue.delete(state.links, id);
+  },
 };
