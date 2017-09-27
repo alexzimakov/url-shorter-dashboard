0a246ffdb51a6f815b8ae0136a4633acb7ff8bc3
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-27 22:31:59 +0300

    Implement `parseTagsString` function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,13 @@
 import getComponentsObject from './getComponentsObject';
+import parseTagsString from './parseTagsString';
 
 const utils = {
   getComponentsObject,
+  parseTagsString,
 };
 
 export default utils;
 export {
   getComponentsObject,
+  parseTagsString,
 };
