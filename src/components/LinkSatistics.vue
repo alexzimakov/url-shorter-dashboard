906d1a9e854792261c699288a559ace1c15c9d9d
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-28 18:19:52 +0300

    Implement LinkStatistics component

---
+++ src/components/LinkSatistics.vue
@@ -0,0 +1,56 @@
+<script>
+import Chart from 'chart.js';
+
+export default {
+  name: 'link-statistics',
+  render(createElement) {
+    return createElement('div', {
+      style: {
+        width: '100%',
+      },
+    }, [
+      createElement('canvas', {
+        attrs: {
+          id: 'test',
+        },
+        ref: 'canvas',
+      }),
+    ]);
+  },
+  props: {
+    chartData: {
+      type: Object,
+      default: {},
+    },
+  },
+  methods: {
+    renderChart(data) {
+      this._chart = new Chart(this.$refs.canvas.getContext('2d'), {
+        type: 'line',
+        data,
+        options: {
+          elements: {
+            line: {
+              tension: 0.00001,
+            },
+          },
+          legend: {
+            display: false,
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              ticks: {
+                stepSize: 1,
+              },
+            }],
+          },
+        },
+      });
+    },
+  },
+  mounted() {
+    this.renderChart(this.chartData);
+  },
+};
+</script>
