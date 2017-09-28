465e1c5b73f469d5502accc4b41de644810626b4
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-28 23:49:59 +0300

    Create _redirects

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
+    _redirects: path.resolve(__dirname, '../_redirects'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
