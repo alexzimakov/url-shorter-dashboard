cc2798ee468dd3aaec005f0665a48ad874665214
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-28 19:25:54 +0300

    Update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import { requireGuest } from '@/utils/auth';
+import { requireAuth, requireGuest } from '@/utils/auth';
 import Home from '@/components/Home';
 import Info from '@/components/Info';
 import Register from '@/components/Register';
 import Login from '@/components/Login';
+import Logout from '@/components/Logout';
 import NotFound from '@/components/NotFound';
 
 Vue.use(Router);
@@ -33,6 +34,12 @@ export default new Router({
       name: 'login',
       component: Login,
       beforeEnter: requireGuest,
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: Logout,
+      beforeEnter: requireAuth,
     },
     {
       path: '*',
