a29aa2b1f5fa528bcafea59826f03ee6c73d7221
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-28 18:21:09 +0300

    Add info route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import { requireGuest } from '@/utils/auth';
 import Home from '@/components/Home';
+import Info from '@/components/Info';
 import Register from '@/components/Register';
 import Login from '@/components/Login';
 
@@ -14,6 +15,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/info/:id',
+      name: 'info',
+      component: Info,
     },
     {
       path: '/register',
