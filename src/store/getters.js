4f1feb171b640110acd3878730501a1e654dfa0a
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-30 13:37:12 +0300

    Update getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,33 @@
+import { normalizeLink } from '@/utils';
+
+
 export default {
   isLoggedIn(state) {
     return !!(state.session);
   },
   authorizationHeader(state) {
+    if (!state.session) {
+      return null;
+    }
+
     return `Bearer ${state.session.token}`;
+  },
+  linksOfLoggedInUser(state) {
+    const author = state.session.user._id;
+
+    return Object.values(state.links.items)
+      .filter(link => link.author === author)
+      .map(link => normalizeLink(link));
+  },
+  filteredLinksByTag(state) {
+    if (!('tags' in state.links.filters)) {
+      return [];
+    }
+
+    const tag = state.links.filters.tags;
+
+    return Object.values(state.links.items)
+      .filter(link => link.tags.indexOf(tag) !== -1)
+      .map(link => normalizeLink(link));
   },
 };
